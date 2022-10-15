b42d3b52cd10d0363e6b5bcf548307678b73ea21
Windel Abuyot Pelayo <pelayowindel@gmail.com>
2022-10-15 16:53:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+  echo "# pelayo-im2-3b" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/wingled22/pelayo-im2-3b.git
+git push -u origin master
   
